daf76b896cc139522c536c86729e08a8caad6b5c
Annaelle Mansuy <annaellemansuy@Annaelles-MacBook-Air.local>
2022-08-22 16:48:07 +0800

    update

--- breadth_First_search.py
+++ breadth_First_search.py
@@ -1,7 +1,6 @@
 
 import numpy as np
 from collections import deque
-from xmlrpc.client import boolean
 
 #Breadth First Search Algorithm
 #scenario finding someone that owns a rolls royce from your list of friendsg
@@ -20,25 +19,25 @@ pplwhohaveRollsRoyce = ["Christopher", "Jane"]
 
 # use queue data structure
 def search(friend):
-  searchqueue = deque() # creates new queue
-  searchqueue += graph[friend]
+  searchqueue = deque() # creates new queue (queue is empty so far)
+  searchqueue += graph[friend] #adds all the people of the person friend list
   AlreadySearched = [] # adds an array of all the people searched before
-  while searchqueue:
+  while searchqueue: # while searchqueue not empty
     friend = searchqueue.popleft() #takes first person off the queue
-    if not friend in AlreadySearched:
+    if not friend in AlreadySearched: # if person is not already searched for rolls royce
       if friendOwns(friend) == True:
         print(friend + " owns a rolls royce!!!!")
         return True
       else:
-        searchqueue += graph[friend]
-        AlreadySearched.append(friend)
+        searchqueue += graph[friend] # add the list of friends' friends to the queue
+        AlreadySearched.append(friend) # add checked name to those list of checked people
   return False
 
 def friendOwns(name):
-  elementExist = name in pplwhohaveRollsRoyce
+  elementExist = name in pplwhohaveRollsRoyce #checks if the name of the person exists in the array of rollsroyce owners
   return elementExist
-# find people who own rolls royce closest to you
-search("Sam")
+# find person who own rolls royce closest to you
+search("me")
 
   
 
